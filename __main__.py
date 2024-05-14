2d276574936c736809e5b753028967273151445a
Ahmed Thahir <ahmedthahir2002@gmail.com>
2024-05-14 21:31:53 +0400

    Re-enable agg in training
    Else, too much space complexity

--- __main__.py
+++ __main__.py
@@ -52,28 +52,6 @@ def plot_summary(df, y, percentage=False):
 def get_summary():
     return pd.read_csv("summary.csv")
 
-def agg(summary):
-    summary = (
-        summary
-        .groupby(["Epoch", "Subset"])
-        .agg(["mean"])
-    )
-    summary.columns = list(map('_'.join, summary.columns.values))
-    summary = (
-        summary
-        .reset_index()
-        .pivot(
-            index="Epoch",
-            columns="Subset",
-            # values = "Accuracy"
-        )
-    )
-    summary.columns = list(map('_'.join, summary.columns.values))
-    summary["Generalization_Gap"] = summary["Loss_mean_Dev"] - summary["Loss_mean_Train"]
-    summary = summary.reset_index()
-    
-    return summary
-
 def main():
     summary = get_summary()
     
